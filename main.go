d4ce05378b8532e17bd87e17738c4b053908a2d2
yangkai <deveye@126.com>
2024-06-27 18:23:48 +0800

    refactor: 重构代码结构

--- main.go
+++ main.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"flag"
-	"log"
-	"mock-server/config"
 	"mock-server/server"
 )
 
@@ -11,28 +9,7 @@ func main() {
 	configPath := flag.String("config", "config.yml", "Path to the configuration file")
 	flag.Parse()
 
-	cfg, err := config.LoadConfig(*configPath)
-	if err != nil {
-		log.Fatalf("Failed to load configuration: %v", err)
-	}
-	err = cfg.InitConfig()
-	if err != nil {
-		log.Fatalf("Failed to init configuration: %v", err)
-	}
-	enabled := cfg.Nacos.Enabled
-	for _, service := range cfg.Server {
-		go func(service config.Server) {
-			if enabled {
-				if err := server.RegisterWithNacos(cfg.Nacos, service.Name, service.Port); err != nil {
-					log.Fatalf("Failed to register with Nacos: %s", err)
-				}
-			}
-			srv := server.New(cfg)
-			if err := srv.Run(service); err != nil {
-				log.Fatalf("Failed to run server: %v", err)
-			}
-		}(*service)
-	}
-	select {}
-
+	srv := server.New()
+	srv.Init(*configPath)
+	srv.Run()
 }
